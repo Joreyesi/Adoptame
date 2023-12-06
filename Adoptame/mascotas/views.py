238643371fc914e76491eb7904801ff8ae6d55b9
Joreyesi <joel_ranger@hotmail.com>
2023-12-06 18:42:16 -0300

    vista mascota

--- Adoptame/mascotas/views.py
+++ Adoptame/mascotas/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# En tu archivo views.py
+
+from .models import Mascota
+
+def listado_animales(request):
+    animales = Mascota.objects.all()
+    return render(request, 'listado_animales.html', {'animales': animales})
