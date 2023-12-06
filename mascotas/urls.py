3093f01ace7f3be236d2f212cfc996fa3c717dd7
Joreyesi <joel_ranger@hotmail.com>
2023-12-06 20:09:57 -0300

    arreglo de url

--- mascotas/urls.py
+++ mascotas/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import listado_animales
 
 urlpatterns = [
-    path('listado_animales/', listado_animales, name='listado_animales'),
+    path('Template/listado_animales/', listado_animales, name='listado_animales'),
     # Otras URL de tu aplicación...
 ]
