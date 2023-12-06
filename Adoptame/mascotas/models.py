d9a6eafeb36e9ad7f706dd48291b66226b1a57c8
Joreyesi <joel_ranger@hotmail.com>
2023-12-06 18:23:30 -0300

    tabla region

--- Adoptame/mascotas/models.py
+++ Adoptame/mascotas/models.py
@@ -98,3 +98,10 @@ class Galeria(models.Model):
 
     def __str__(self):
         return self.nombre_c
+    
+
+    class Region(models.Model):
+    nombre_r = models.CharField(max_length=100, primary_key=True)
+
+    def __str__(self):
+        return self.nombre_r
