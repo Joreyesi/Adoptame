ce490206a20f5e45876ac708580be7e4c5a56833
Joreyesi <joel_ranger@hotmail.com>
2023-12-06 19:50:28 -0300

    mas cambios a los modelos

--- mascotas/models.py
+++ mascotas/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class Mascota(models.Model):
     id_mascotas = models.AutoField(primary_key=True)
-    usuario = models.ForeignKey('mascotas.Usuario', related_name='mascotas', on_delete=models.CASCADE)
+    usuario = models.ForeignKey('mascotas.Usuario', related_name='mascotas', on_delete=models.CASCADE)  # Asegúrate de reemplazar 'TuApp' con el nombre real de tu aplicación.
     nombre_m = models.CharField(max_length=100)
     raza_m = models.CharField(max_length=100)
     vacuna_m = models.BooleanField(default=False)
@@ -16,7 +16,6 @@ class Mascota(models.Model):
     comportamiento_m = models.TextField()
     peso_m = models.FloatField()
     especie_m = models.CharField(max_length=50)
-
     class Meta:
         app_label = 'mascotas'
 
@@ -42,7 +41,7 @@ class Usuario(AbstractUser):
 
 class Organizacion(models.Model):
     id_org_o = models.AutoField(primary_key=True)
-    usuario = models.ForeignKey('mascotas.Usuario', related_name='mascotas', on_delete=models.CASCADE)  # Asegúrate de reemplazar 'TuApp' con el nombre real de tu aplicación.
+    usuario = models.ForeignKey('mascotas.Usuario', on_delete=models.CASCADE)  # Asegúrate de reemplazar 'TuApp' con el nombre real de tu aplicación.
     personal_o = models.IntegerField()
     mascotas_o = models.IntegerField()
     ciudad_o = models.CharField(max_length=100)
@@ -57,7 +56,7 @@ class Organizacion(models.Model):
     
 class Estado(models.Model):
     id_mascotas_e = models.AutoField(primary_key=True)
-    id_mascota = models.ForeignKey('mascotas.Mascota', related_name='mascotas', on_delete=models.CASCADE)  # Asegúrate de reemplazar 'TuApp' con el nombre real de tu aplicación.
+    id_mascota = models.ForeignKey('mascotas.Mascota', on_delete=models.CASCADE)  # Asegúrate de reemplazar 'TuApp' con el nombre real de tu aplicación.
     vacunas_e = models.BooleanField()
     tipo_vacuna_e = models.CharField(max_length=50, blank=True, null=True)
     tratamiento_e = models.TextField(blank=True, null=True)
@@ -70,7 +69,7 @@ class Estado(models.Model):
 
 class Publicacion(models.Model):
     id_p = models.AutoField(primary_key=True)
-    id_org_o = models.ForeignKey('mascotas.Organizacion', related_name='mascotas', on_delete=models.CASCADE)  # Asegúrate de reemplazar 'TuApp' con el nombre real de tu aplicación.
+    id_org_o = models.ForeignKey('mascotas.Organizacion', on_delete=models.CASCADE)  # Asegúrate de reemplazar 'TuApp' con el nombre real de tu aplicación.
     nombres_p = models.CharField(max_length=100)
     nombre_mascota_p = models.CharField(max_length=100)
     fecha_p = models.DateField()
@@ -83,7 +82,7 @@ class Publicacion(models.Model):
 
 class Galeria(models.Model):
     id_galeria = models.AutoField(primary_key=True)
-    id_mascota = models.ForeignKey('mascotas.Mascota', related_name='mascotas', on_delete=models.CASCADE)  # Asegúrate de reemplazar 'TuApp' con el nombre real de tu aplicación.
+    id_mascota = models.ForeignKey('mascotas.Mascota', on_delete=models.CASCADE)  # Asegúrate de reemplazar 'TuApp' con el nombre real de tu aplicación.
     cantidad_fotos_g = models.IntegerField()
     descripcion_g = models.TextField(blank=True, null=True)
     tipo_mascota_g = models.CharField(max_length=50)
@@ -95,9 +94,9 @@ class Galeria(models.Model):
 
 class Comuna(models.Model):
     nombre_c = models.CharField(max_length=100, primary_key=True)
-    nombre_r = models.ForeignKey('mascotas.Region', related_name='mascotas', on_delete=models.CASCADE)  # Asegúrate de reemplazar 'TuApp' con el nombre real de tu aplicación.
-    rut_usuario = models.ForeignKey('mascotas.Usuario', related_name='mascotas', on_delete=models.CASCADE)  # Asegúrate de reemplazar 'TuApp' con el nombre real de tu aplicación.
-    id_org_o = models.ForeignKey('mascotas.Organizacion', related_name='mascotas', on_delete=models.CASCADE, blank=True, null=True)  # Asegúrate de reemplazar 'TuApp' con el nombre real de tu aplicación.
+    nombre_r = models.ForeignKey('mascotas.Region', on_delete=models.CASCADE)  # Asegúrate de reemplazar 'TuApp' con el nombre real de tu aplicación.
+    rut_usuario = models.ForeignKey('mascotas.Usuario', on_delete=models.CASCADE)  # Asegúrate de reemplazar 'TuApp' con el nombre real de tu aplicación.
+    id_org_o = models.ForeignKey('mascotas.Organizacion', on_delete=models.CASCADE, blank=True, null=True)  # Asegúrate de reemplazar 'TuApp' con el nombre real de tu aplicación.
 
     def __str__(self):
         return self.nombre_c
