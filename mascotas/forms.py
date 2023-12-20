f07239c22cbb72a03f463f5685d4f982bdf5dab3
Joreyesi <joel_ranger@hotmail.com>
2023-12-20 10:58:35 -0300

    arreglo al form

--- mascotas/forms.py
+++ mascotas/forms.py
@@ -6,10 +6,9 @@ from .models import Mascota
 class MascotaForm(forms.ModelForm):
     class Meta:
         model = Mascota
-        fields = '__all__'
+        exclude = ['rut_usuario']
         widgets = {
-            'fecha_nac_m': forms.DateInput(attrs={'type': 'date'}),
+            'fecha_nac_m': forms.TextInput(attrs={'type': 'date'}),
             # Puedes agregar más personalizaciones aquí según tus necesidades
         }
 
-# Otros formularios pueden ir aquí si es necesario
