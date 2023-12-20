aa9b601a4c806bdf15502bf6f8ffd3515365b423
Joreyesi <joel_ranger@hotmail.com>
2023-12-20 09:55:27 -0300

    path para mascota

--- mascotas/urls.py
+++ mascotas/urls.py
@@ -2,8 +2,15 @@
 
 from django.urls import path
 from .views import listado_animales
+from .views import ingresar_mascota
 
 urlpatterns = [
     path('templates/listado_animales/', listado_animales, name='listado_animales'),
+    # Otras rutas pueden ir aquí
+]
+
+
+urlpatterns = [
+    path('ingresar_mascota/', ingresar_mascota, name='ingresar_mascota'),
     # Otras rutas pueden ir aquí
 ]
