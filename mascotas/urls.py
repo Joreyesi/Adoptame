cbcd1b482342800f1985c6e443260c84ff4b3adc
Joreyesi <joel_ranger@hotmail.com>
2023-12-20 10:01:02 -0300

    update url app

--- mascotas/urls.py
+++ mascotas/urls.py
@@ -1,16 +1,10 @@
 # mascotas/urls.py
 
 from django.urls import path
-from .views import listado_animales
-from .views import ingresar_mascota
+from .views import listado_mascotas, ingresar_mascota
 
 urlpatterns = [
-    path('templates/listado_animales/', listado_animales, name='listado_animales'),
-    # Otras rutas pueden ir aquí
-]
-
-
-urlpatterns = [
+    path('listado_mascotas/', listado_mascotas, name='listado_mascotas'),
     path('ingresar_mascota/', ingresar_mascota, name='ingresar_mascota'),
     # Otras rutas pueden ir aquí
 ]
