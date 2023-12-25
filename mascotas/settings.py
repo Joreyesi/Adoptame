3be68e1fb3717c95b5282eaf139e6bd43d0ddfc9
Joreyesi <joel_ranger@hotmail.com>
2023-12-25 15:35:02 -0300

    cambio al settings media

--- mascotas/settings.py
+++ mascotas/settings.py
@@ -65,7 +65,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Configuración de archivos multimedia
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media', 'mascotas')
 
 # Zona horaria y configuración internacional
 TIME_ZONE = 'UTC'
