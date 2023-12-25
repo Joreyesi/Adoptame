62ddd43f85ff703031e20470584ddbb014b27231
Joreyesi <joel_ranger@hotmail.com>
2023-12-25 18:54:04 -0300

    cambio en urls

--- mascotas/urls.py
+++ mascotas/urls.py
@@ -1,11 +1,9 @@
 # mascotas/urls.py
-
 from django.urls import path
-from .views import listado_mascotas, ingresar_mascota, eliminar_mascota
+from . import views
 
 urlpatterns = [
-    path('listado_mascotas/', listado_mascotas, name='listado_mascotas'),
-    path('ingresar_mascota/', ingresar_mascota, name='ingresar_mascota'),
-    path('eliminar_mascota/<int:mascota_id>/', eliminar_mascota, name='eliminar_mascota'),
-    # Otras rutas pueden ir aquí
+    path('listado_mascotas/', views.listado_mascotas, name='listado_mascotas'),
+    path('ingresar_mascota/', views.ingresar_mascota, name='ingresar_mascota'),
+    path('eliminar_mascota/<int:mascota_id>/', views.eliminar_mascota, name='eliminar_mascota'),
 ]
--- urls.py
+++ urls.py
@@ -1,5 +1,4 @@
-# tu_proyecto/urls.py
-
+# mascotas_project/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -7,10 +6,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mascotas.urls')),  # Use una ruta vacía para la aplicación principal
-    # Otras rutas pueden ir aquí
+    path('', include('mascotas.urls')),  # Incluye las URLs de la aplicación 'mascotas'
 ]
 
-# Agrega estas líneas al final para servir archivos multimedia durante el desarrollo
+# Configuración para servir archivos de medios durante el desarrollo
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
