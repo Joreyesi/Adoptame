69617890c4eee008a85c3587a2f7d72593806edb
Joreyesi <joel_ranger@hotmail.com>
2023-12-25 15:41:07 -0300

    fix a la ruta en model

--- mascotas/models.py
+++ mascotas/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
 import datetime
+import os
 
 # Create your models here.
 class Mascota(models.Model):
