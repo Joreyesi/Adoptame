5c9037d23d1b9a04f94bce98276c67c0d984ca64
Joreyesi <joel_ranger@hotmail.com>
2023-12-25 19:44:36 -0300

    cambios path url proyecto

--- urls.py
+++ urls.py
@@ -1,14 +1,14 @@
-# mascotas_project/urls.py
-from django.contrib import admin
-from django.urls import path, include
+# En tu archivo urls.py del proyecto
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mascotas.urls')),  # Incluye las URLs de la aplicación 'mascotas'
+    path('mascotas/', include('mascotas.urls')),  # Incluye las URLs de tu aplicación 'mascotas'
 ]
 
-# Configuración para servir archivos de medios durante el desarrollo
+# Configuración para servir archivos multimedia durante el desarrollo
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
