ea637d3de3b8c500b009200522f8ff018db48ace
Joreyesi <joel_ranger@hotmail.com>
2023-12-25 13:59:23 -0300

    update archivo admin

--- mascotas/admin.py
+++ mascotas/admin.py
@@ -1,6 +1,13 @@
 # mascotas/admin.py
 
 from django.contrib import admin
-from .models import Mascota
+from .models import Mascota, Usuario, Organizacion, Estado, Publicacion, Galeria, Comuna, Region
 
 admin.site.register(Mascota)
+admin.site.register(Usuario)
+admin.site.register(Organizacion)
+admin.site.register(Estado)
+admin.site.register(Publicacion)
+admin.site.register(Galeria)
+admin.site.register(Comuna)
+admin.site.register(Region)
