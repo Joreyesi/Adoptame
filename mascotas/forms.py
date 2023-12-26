a2cad6443f25ef52d0ec5040e10011fe7b65cf24
Joreyesi <joel_ranger@hotmail.com>
2023-12-25 21:32:01 -0300

    se agrega calendario al forms

--- mascotas/forms.py
+++ mascotas/forms.py
@@ -4,6 +4,27 @@ from django import forms
 from django.forms import SelectDateWidget
 from .models import Mascota
 from datetime import date
+import calendar
+
+
+class CustomSelectDateWidget(SelectDateWidget):
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+
+        # Obtener los valores actuales para año, mes y día
+        year = context['widget']['year']
+        month = context['widget']['month']
+        day = context['widget']['day']
+
+        # Obtener el número máximo de días para el mes y año actual
+        max_days = calendar.monthrange(year, month)[1]
+
+        # Ajustar la cantidad de días en el widget
+        context['widget']['days'] = [
+            (i, '%02d' % i) for i in range(1, max_days + 1)
+        ]
+
+        return context
 
 class MascotaForm(forms.ModelForm):
     class Meta:
@@ -11,7 +32,7 @@ class MascotaForm(forms.ModelForm):
         exclude = ['rut_usuario']
         today = date.today()
         widgets = {
-            'fecha_nac_m': SelectDateWidget(years=range(2000, today.year + 1))
+            'fecha_nac_m': CustomSelectDateWidget(years=range(2000, today.year + 1))
         }
 
     # Agrega este método para manejar el campo de imagen
