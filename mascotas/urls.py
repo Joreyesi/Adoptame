5f538dbd82d7fdd486d7b568471ba7d91c1c5944
Joreyesi <joel_ranger@hotmail.com>
2023-12-27 13:44:38 -0300

    cambio a las urls

--- mascotas/urls.py
+++ mascotas/urls.py
@@ -1,10 +1,12 @@
 # mascotas/urls.py
 from django.urls import path
 from . import views
+from django.contrib import admin
 
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('listado_mascotas/', views.listado_mascotas, name='listado_mascotas'),
     path('ingresar_mascota/', views.ingresar_mascota, name='ingresar_mascota'),
     path('eliminar_mascota/<int:mascota_id>/', views.eliminar_mascota, name='eliminar_mascota'),
