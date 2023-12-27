65ab32e56f8e70061af6589abd95ec465622454e
Joreyesi <joel_ranger@hotmail.com>
2023-12-27 11:17:29 -0300

    fix url proyecto

--- urls.py
+++ urls.py
@@ -3,13 +3,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import url
+
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     path('mascotas/', include('mascotas.urls', namespace='mascotas')),
 ]
 
 # Configuración para servir archivos multimedia durante el desarrollo
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
